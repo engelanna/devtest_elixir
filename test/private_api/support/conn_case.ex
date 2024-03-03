4103548fb2556bd1afd92d0da2b745ff23ce06c6
engelanna <13955209+engelanna@users.noreply.github.com>
2024-03-03 04:16:16 +0100

    Structure the /test dir properly

--- test/private_api/support/conn_case.ex
+++ test/private_api/support/conn_case.ex
@@ -1,4 +1,4 @@
-defmodule PrivateAPI.ConnCase do
+defmodule PrivateAPI.Support.ConnCase do
   @moduledoc """
   This module defines the test case to be used by
   tests that require setting up a connection.
