c33f7924bd32c7579b2c2dad01e32cd686fe42fe
engelanna <13955209+engelanna@users.noreply.github.com>
2024-03-05 08:50:48 +0100

    Add 'faker' dependency

--- mix.exs
+++ mix.exs
@@ -53,7 +53,8 @@ defmodule DevtestElixir.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:faker, "~> 0.18.0"}
     ]
   end
 
