38de6ea0c46069ec99118fbb869c7d72a6111087
engelanna <13955209+engelanna@users.noreply.github.com>
2024-04-13 00:30:26 +0200

    Get endpoint ports from env vars (runtime.exs)

--- config/runtime.exs
+++ config/runtime.exs
@@ -62,7 +62,7 @@ if config_env() == :prod do
       # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+      port: System.get_env("PRIVATE_API_PORT")
     ],
     secret_key_base: secret_key_base
 
@@ -74,7 +74,7 @@ if config_env() == :prod do
       # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+      port: System.get_env("PUBLIC_API_PORT")
     ],
     secret_key_base: secret_key_base
 
