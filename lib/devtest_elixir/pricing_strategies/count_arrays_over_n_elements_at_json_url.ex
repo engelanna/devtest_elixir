cd1fe62871c240414cac67fece40718b7e67180f
engelanna <13955209+engelanna@users.noreply.github.com>
2024-05-02 19:12:53 +0200

    Declutter CountArraysOverNElementsAtJsonUrl#count_arrays

--- lib/devtest_elixir/pricing_strategies/count_arrays_over_n_elements_at_json_url.ex
+++ lib/devtest_elixir/pricing_strategies/count_arrays_over_n_elements_at_json_url.ex
@@ -10,21 +10,19 @@ defmodule DevtestElixir.PricingStrategies.CountArraysOverNElementsAtJsonUrl do
     with {:ok, %HTTPoison.Response{status_code: 200, body: body}} <- get(url),
          json_object <- Jason.decode!(body)
     do
-      _price = count_arrays(json_object, min_array_length)
+      _price = json_array_count(json_object, min_array_length)
     end
   end
 
 
-  defp count_arrays(input_json, min_array_length) when is_list(input_json) do
+  defp json_array_count(input_json, min_array_length) when is_list(input_json) do
     Enum.reduce(input_json, 0, fn input_list_element, count_so_far ->
-        is_an_interesting_list = is_list(input_list_element) && length(input_list_element) >= min_array_length
-
         cond do
-          is_an_interesting_list ->
-            count_so_far + count_arrays(input_list_element, min_array_length) + 1
+          an_interesting_list?(input_list_element, min_array_length) ->
+            count_so_far + json_array_count(input_list_element, min_array_length) + 1
 
           is_list(input_list_element) || is_map(input_list_element) ->
-            count_so_far + count_arrays(input_list_element, min_array_length)
+            count_so_far + json_array_count(input_list_element, min_array_length)
 
           true -> count_so_far
         end
@@ -32,10 +30,14 @@ defmodule DevtestElixir.PricingStrategies.CountArraysOverNElementsAtJsonUrl do
     )
   end
 
-  defp count_arrays(input_json, min_array_length) when is_map(input_json) do
-    count_arrays(
+  defp json_array_count(input_json, min_array_length) when is_map(input_json) do
+    json_array_count(
       Map.values(input_json),
       min_array_length
     )
+  end
+
+  defp an_interesting_list?(potential_list, min_interesting_length) do
+    is_list(potential_list) && length(potential_list) >= min_interesting_length
   end
 end
